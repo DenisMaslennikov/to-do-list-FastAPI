874373787afa9aba405c18cdac33f6451b7f5b6c
Denis Maslennikov <ktotom7@yandex.ru>
2024-10-25 09:11:19 +0300

    add to swagger responses codes for errors

--- backend/app/api/v1/users/views.py
+++ backend/app/api/v1/users/views.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.api.v1.auth.jwt import create_refresh_token, create_access_token, decode_token
 from app.api.v1.dependencies.jwt import user_id_from_refresh_token
@@ -24,7 +25,10 @@ from app.db.models import User
 router = APIRouter(tags=["Users"])
 
 
-@router.post("/jwt/create/")
+@router.post(
+    "/jwt/create/",
+    responses={status.HTTP_401_UNAUTHORIZED: {"description": "Ошибка авторизации"}},
+)
 async def create_tokens(user: Annotated[User, Depends(auth_user)]) -> JWTTokensPairWithTokenType:
     """Логин пользователя для получения JWT токенов."""
     return JWTTokensPairWithTokenType(
@@ -33,7 +37,10 @@ async def create_tokens(user: Annotated[User, Depends(auth_user)]) -> JWTTokensP
     )
 
 
-@router.post("/jwt/refresh/")
+@router.post(
+    "/jwt/refresh/",
+    responses={status.HTTP_401_UNAUTHORIZED: {"description": "Некорректный refresh токен"}},
+)
 async def tokens_refresh(user_id: Annotated[UUID, Depends(user_id_from_refresh_token)]) -> JWTTokensPairWithTokenType:
     """Обновление токенов по refresh токену."""
     return JWTTokensPairWithTokenType(
@@ -53,25 +60,49 @@ async def token_validate(token: JWTTokenForValidation) -> TokenValidationResult:
         return TokenValidationResult(validation_result=True)
 
 
-@router.post("/register/", response_model=ReadUser)
+@router.post(
+    "/register/",
+    response_model=ReadUser,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"description": "Пользователь с таким Username или Email уже зарегистрирован"},
+    },
+)
 async def user_register(
     new_user_data: CreateUser, session: Annotated[AsyncSession, Depends(db_helper.get_session)]
 ) -> User:
     """Создание нового пользователя."""
+    user_from_bd = await crud.get_user_by_email_or_username_repo(session, new_user_data.email, new_user_data.username)
+    if user_from_bd is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email or Username already registered")
     user = await crud.create_user_repo(session=session, new_user_data=new_user_data)
     return user
 
 
-@router.get("/me/", response_model=ReadUser)
+@router.get(
+    "/me/",
+    response_model=ReadUser,
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Пользователь не найден в базе данных"},
+        status.HTTP_401_UNAUTHORIZED: {"description": "Ошибка авторизации"},
+    },
+)
 async def get_user_me(user: Annotated[User, Depends(get_current_user)]) -> User:
     """Получает информацию о текущем пользователе."""
     return user
 
 
-@router.delete("/me/", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    "/me/",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_description="Пользователь удален",
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Пользователь не найден в базе данных"},
+        status.HTTP_401_UNAUTHORIZED: {"description": "Ошибка авторизации"},
+    },
+)
 async def delete_user_me(
     user: Annotated[User, Depends(get_current_user)],
     session: Annotated[AsyncSession, Depends(db_helper.get_session)],
 ) -> None:
     """Удаляет текущего пользователя."""
-    await delete_user_repo(session, user)
+    await crud.delete_user_repo(session, user)
