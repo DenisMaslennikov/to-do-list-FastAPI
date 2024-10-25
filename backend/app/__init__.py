916e3c10b56a94ce66e2d294d9064a33f64a480d
Denis Maslennikov <ktotom7@yandex.ru>
2024-10-25 13:07:44 +0300

    add lifespan for fastapi app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api import api_router
 from app.config import settings
+from app.db import db_helper
 
-main_app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> None:
+    """Жизненный цикл приложения."""
+    yield
+
+    db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(api_router, prefix=settings.api.prefix)
